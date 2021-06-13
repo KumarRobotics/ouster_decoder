2ab75829606fe608978006b3c47a4bbbd90902bd
chao <quchao@seas.upenn.edu>
2021-06-12 22:32:15 -0400

    add benchmark for transform

--- src/bench.cpp
+++ src/bench.cpp
@@ -1,7 +1,10 @@
 #include <benchmark/benchmark.h>
+#include <pcl/common/transforms.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <tbb/parallel_for.h>
+
+#include <Eigen/Geometry>
 
 namespace {
 
@@ -60,7 +63,7 @@ void ProcessCloudColMajor(CloudT<PointT>& cloud) {
 template <typename PointT>
 void ProcessCloudColMajorPar(CloudT<PointT>& cloud) {
   tbb::parallel_for(tbb::blocked_range<int>(0, cloud.width),
-                    [&cloud](tbb::blocked_range<int> range) {
+                    [&cloud](const tbb::blocked_range<int>& range) {
                       for (int j = range.begin(); j < range.end(); ++j) {
                         for (int i = 0; i < cloud.height; ++i) {
                           auto& p = cloud.at(j, i);
@@ -120,6 +123,125 @@ BENCHMARK_TEMPLATE(BM_ProcessCloud, PointXYZRGB, ColMajor, Serial)
     ->RangeMultiplier(2)
     ->Range(kMinWidth, kMaxWidth);
 BENCHMARK_TEMPLATE(BM_ProcessCloud, PointXYZRGB, ColMajor, Parallel)
+    ->RangeMultiplier(2)
+    ->Range(kMinWidth, kMaxWidth);
+
+template <typename PointT>
+void TransformCloud(CloudT<PointT>& cloud, const Eigen::Affine3f& transform) {
+  CloudT<PointT> out;
+  pcl::transformPointCloud(cloud, out, transform);
+}
+
+void BM_TransformCloudBase(benchmark::State& state) {
+  CloudXYZ cloud(kHeight, state.range(0));
+  Eigen::Affine3f tf = Eigen::Affine3f::Identity();
+
+  for (auto _ : state) {
+    TransformCloud(cloud, tf);
+  }
+}
+BENCHMARK(BM_TransformCloudBase)
+    ->RangeMultiplier(2)
+    ->Range(kMinWidth, kMaxWidth);
+
+template <typename PointT>
+void TransformCloudRowMajor(CloudT<PointT>& cloud,
+                            const std::vector<Eigen::Affine3f>& transforms) {
+  for (int i = 0; i < cloud.height; ++i) {
+    for (int j = 0; j < cloud.width; ++j) {
+      const auto& tf = transforms[j];
+      auto& pt = cloud.at(j, i);
+      auto p = pt.getArray3fMap();
+      p = tf * p;
+    }
+  }
+}
+
+template <typename PointT>
+void TransformCloudRowMajorPar(CloudT<PointT>& cloud,
+                               const std::vector<Eigen::Affine3f>& transforms) {
+  tbb::parallel_for(tbb::blocked_range<int>(0, cloud.height),
+                    [&](const tbb::blocked_range<int>& range) {
+                      for (int i = range.begin(); i < range.end(); ++i) {
+                        for (int j = 0; j < cloud.width; ++j) {
+                          const auto& tf = transforms[j];
+                          auto& pt = cloud.at(j, i);
+                          auto p = pt.getArray3fMap();
+                          p = tf * p;
+                        }
+                      }
+                    });
+}
+
+template <typename Execute>
+void BM_TransformCloudRowMajor(benchmark::State& state) {
+  CloudXYZ cloud(kHeight, state.range(0));
+  std::vector<Eigen::Affine3f> transforms(state.range(0),
+                                          Eigen::Affine3f::Identity());
+
+  for (auto _ : state) {
+    if constexpr (std::is_same_v<Execute, Serial>) {
+      TransformCloudRowMajor(cloud, transforms);
+    } else {
+      TransformCloudRowMajorPar(cloud, transforms);
+    }
+  }
+}
+
+template <typename PointT>
+void TransformCloudColMajor(CloudT<PointT>& cloud,
+                            const std::vector<Eigen::Affine3f>& transforms) {
+  for (int i = 0; i < cloud.height; ++i) {
+    const auto& tf = transforms[i];
+    for (int j = 0; j < cloud.width; ++j) {
+      auto& pt = cloud.at(j, i);
+      auto p = pt.getArray3fMap();
+      p = tf * p;
+    }
+  }
+}
+
+template <typename PointT>
+void TransformCloudColMajorPar(CloudT<PointT>& cloud,
+                               const std::vector<Eigen::Affine3f>& transforms) {
+  tbb::parallel_for(tbb::blocked_range<int>(0, cloud.height),
+                    [&](const tbb::blocked_range<int>& range) {
+                      for (int i = range.begin(); i < range.end(); ++i) {
+                        const auto& tf = transforms[i];
+                        for (int j = 0; j < cloud.width; ++j) {
+                          auto& pt = cloud.at(j, i);
+                          auto p = pt.getArray3fMap();
+                          p = tf * p;
+                        }
+                      }
+                    });
+}
+
+template <typename Execute>
+void BM_TransformCloudColMajor(benchmark::State& state) {
+  CloudXYZ cloud(state.range(0), kHeight);
+  std::vector<Eigen::Affine3f> transforms(state.range(0),
+                                          Eigen::Affine3f::Identity());
+
+  for (auto _ : state) {
+    if constexpr (std::is_same_v<Execute, Serial>) {
+      TransformCloudColMajor(cloud, transforms);
+    } else {
+      TransformCloudColMajorPar(cloud, transforms);
+    }
+  }
+}
+
+BENCHMARK_TEMPLATE(BM_TransformCloudRowMajor, Serial)
+    ->RangeMultiplier(2)
+    ->Range(kMinWidth, kMaxWidth);
+BENCHMARK_TEMPLATE(BM_TransformCloudColMajor, Serial)
+    ->RangeMultiplier(2)
+    ->Range(kMinWidth, kMaxWidth);
+BENCHMARK_TEMPLATE(BM_TransformCloudRowMajor, Parallel)
+    ->RangeMultiplier(2)
+    ->Range(kMinWidth, kMaxWidth);
+BENCHMARK_TEMPLATE(BM_TransformCloudColMajor, Parallel)
     ->RangeMultiplier(2)
     ->Range(kMinWidth, kMaxWidth);
 
